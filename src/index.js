083f091d86cc59d4c1ed33d82a5c9a5fc523017c
Rizardomon <57680110+Rizardomon@users.noreply.github.com>
2020-07-28 14:07:26 -0300

    Adição rotas do projeto

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact/>
+      <Route component={
+        () => ( <div>Página 404</div> )
+      }/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
