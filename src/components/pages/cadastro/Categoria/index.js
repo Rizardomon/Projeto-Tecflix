777e223c61d9d5e4d5666bb335b1494d2ce612d4
Rizardomon <57680110+Rizardomon@users.noreply.github.com>
2020-07-29 11:53:33 -0300

    funcionalidade de adição de categorias

--- src/components/pages/cadastro/Categoria/index.js
+++ src/components/pages/cadastro/Categoria/index.js
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Insira a Categoria');
+
   return (
-      <PageDefault>
-          <h1>Cadastro de Categoria</h1>
+    <PageDefault>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-          <form>
+        <form onSubmit={ (e) => {
+          e.preventDefault();
+          setCategorias([
+            ...categorias,
+            nomeDaCategoria
+          ])
+          } }>
 
-            <label>
+          <label>
             Nome da Categoria:
-                <input
-                    type="text"
-                />
-            </label>
+            <input
+              type="text"
+              placeholder={nomeDaCategoria}
+              onChange={ (e) => {
+              setNomeDaCategoria(e.target.value);
+              } }
+            />
+          </label>
 
-            <button>
-                Cadastrar
-            </button>
+          <button>
+            Cadastrar
+          </button>
+
         </form>
 
-          <Link to='/'>
-              Ir para home
-          </Link>
-      </PageDefault>
+        <ul>
+          {categorias.map((categoria) => {
+            return (
+              <li key={categoria}>
+                {categoria}
+              </li>
+            );
+          })}
+        </ul>
+
+        <Link to='/'>
+          Ir para home
+        </Link>
+    </PageDefault>
   );
 }
 
